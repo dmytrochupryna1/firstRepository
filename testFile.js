5056321c0727c8b11b6f87a185cddb33057f9e22
dmytrochupryna1 <128016969+dmytrochupryna1@users.noreply.github.com>
2023-04-25 21:56:28 +0700

    created new test file, added dogOwners variable

---
+++ testFile.js
@@ -0,0 +1,14 @@
+const dogOwners = {
+    name: "Dima",
+    age: 25,
+    dog: {
+        name: "Bobik",
+        age: 3,
+        breed: "Labrador"
+    },
+    dog2: {
+        name: "Sharik",
+        age: 5,
+        breed: "Pitbull"
+    }
+}
